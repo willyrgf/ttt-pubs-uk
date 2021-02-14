607097771646baddb191e3a8883ea711dc71a794
Willy Romão <willyr.goncalves@gmail.com>
2021-02-14 19:35:04 +0000

    add a seeder to part4

---
+++ seeders/part4_seeder.sql
@@ -0,0 +1,7 @@
+-- creating some comments
+select ttt.store_user_pub_rating('willy','Duke of cumberland', 70);
+select ttt.store_user_pub_rating('romao','Duke of cumberland', 80);
+select ttt.store_user_pub_rating('mauricio','Duke of cumberland', 90);
+select ttt.store_user_pub_rating('willy','Simmons', 70);
+select ttt.store_user_pub_rating('romao','Simmons', 80);
+select ttt.store_user_pub_rating('mauricio','Simmons', 90);
